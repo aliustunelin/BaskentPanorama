1d5cbb5453cbfdff67014c283c6e48c38c86c9f2
Your Name <you@example.com>
2021-02-18 06:55:54 +0300

    version1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BaskentPanorama
+
+	Bu Proje Dünya Kentlerinden İstanbul, Atina, Londra, New York vs. Şehirlerin Panoramik Görüntülerinin Gösterildiği Bir WEb Sitesi Olarak Tasarlanmıştır.
---
+++ js/script2.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('#container');
+
+const panorama = new PANOLENS.ImagePanorama( '../img/atina.jpg' );
+const panorama2 = new PANOLENS.ImagePanorama( '../img/london.jpg' );
+
+
+
+
+const infospot = new PANOLENS.Infospot(300, PANOLENS.DataImage.Info);
+infospot.position.set(0,0,-7000);
+infospot.addHoverText('London!', 40);
+infospot.addEventListener( 'click', function(){
+    viewer.setPanorama( panorama2 );
+  } );
+
+
+
+const viewer = new PANOLENS.Viewer({container:container, 
+    //    autoRotate: true
+    });
+    
+panorama.add(infospot);
+
+viewer.add(panorama);    
+
+viewer.add( panorama, panorama2 );
+
+viewer.addUpdateCallback(function(){
+  
+});
