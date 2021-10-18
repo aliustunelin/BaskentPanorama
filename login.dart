fb4a996a6b6221c284612154e7549e22f1c044cc
aliustunelin <40759486+aliustunelin@users.noreply.github.com>
2021-10-19 00:12:35 +0300

    Create login.dart

---
+++ login.dart
@@ -0,0 +1,90 @@
+//ali
+
+import 'package:dhbt_okulu/constants/color.dart';
+import 'package:dhbt_okulu/screen/register.dart';
+import 'package:dhbt_okulu/widgets/login_text_form_field.dart';
+import 'package:dhbt_okulu/widgets/navbar.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Giriş",
+          style: TextStyle(color: FixedColor().color3),
+        ),
+        centerTitle: true,
+        backgroundColor: FixedColor().color1,
+        iconTheme: IconThemeData(
+          color: FixedColor().color3,
+        ),
+        shadowColor: FixedColor().color1,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              "Hoşgeldiniz!",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          LoginTexFormFiel(labelTextnum: 2,),
+          LoginTexFormFiel(labelTextnum: 4,),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => const Navbar()));
+                },
+                child: const Text("Giriş Yap", style: TextStyle(fontSize: 17)),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  primary: FixedColor().color3,
+                  fixedSize: Size(MediaQuery.of(context).size.width * 0.9,
+                      MediaQuery.of(context).size.height * 0.055),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Hesabın yok mu?",
+                style: TextStyle(color: FixedColor().color2),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => const Register()));
+                },
+                child: Text(
+                  "Kayıt Ol",
+                  style: TextStyle(color: FixedColor().color3),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
